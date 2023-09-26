d3e1904638ac38bc4cf83fec7f815e017021158e
Toluwase <toluwase@LIBERTYs-MacBook-Pro.local>
2023-09-26 23:34:52 +0100

    feat: new CID

--- ipfs/upload.js
+++ ipfs/upload.js
@@ -1,36 +1,38 @@
 async function run() {
-    const { create } = await import('ipfs-http-client');
-    const ipfs = await create();
-    
-    // we added three attributes, add as many as you want!
-    const metadata = {
-        path: '/',
-        content: JSON.stringify({
-            name: "My First NFT",
-            attributes: [
-            {
-                "trait_type": "Peace",
-                "value": "10" 
-            },
-            {
-                "trait_type": "Love",
-                "value": "100"
-            },
-            {
-                "trait_type": "Web3",
-                "value": "1000"
-            }
-            ],
-            // update the IPFS CID to be your image CID
-            image: "https://ipfs.io/ipfs/QmQ2wnwaFJ1w42UTywTWpM8RgiqrWwKFR6AMrpyiHPgi3p",
-            description: "So much PLW3!"
-        })
-    };
+  const { create } = await import("ipfs-http-client");
+  const ipfs = await create();
 
-    const result = await ipfs.add(metadata);
-    console.log(result);
+  // we added three attributes, add as many as you want!
+  const metadata = {
+    path: "/",
+    content: JSON.stringify({
+      name: "My First NFT",
+      attributes: [
+        {
+          trait_type: "Peace",
+          value: "10",
+        },
+        {
+          trait_type: "Love",
+          value: "100",
+        },
+        {
+          trait_type: "Web3",
+          value: "1000",
+        },
+      ],
+      cid: "QmZrM1fmZiYR5cfgBnrwtFbZUNitnnr7M8d2N7312arrWk",
+      // update the IPFS CID to be your image CID
+      image:
+        "https://ipfs.io/ipfs/QmZrM1fmZiYR5cfgBnrwtFbZUNitnnr7M8d2N7312arrWk?filename=16835763-young-man-climbing-on-a-limestone-wall-with-wide-valley-on-the-background.jpg",
+      description: "So much PLW3!",
+    }),
+  };
 
-    process.exit(0);
+  const result = await ipfs.add(metadata);
+  console.log(result);
+
+  process.exit(0);
 }
 
 run();
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,20 @@
 async function main() {
   // if you changed the name of the contract, be sure to update this here!
-  const MyToken = await hre.ethers.getContractFactory("MyToken");
+  const MyToken = await hre.ethers.getContractFactory("Tywo");
 
   const nft = await MyToken.deploy();
 
   await nft.deployed();
 
   console.log("NFT deployed to:", nft.address);
- 
+
   // mint one to yourself!
   const signer0 = await ethers.provider.getSigner(0);
   // update the IPFS CID to be your metadata CID
-  await nft.safeMint(await signer0.getAddress(), "ipfs://QmbCwoJB7g9b86BfEahsjzyzzNx3WEu7Jc1opXWgeTXFgb");
+  await nft.safeMint(
+    await signer0.getAddress(),
+    "ipfs://QmfBGeuxbghWxE5QXvaAbhmcjtrAtwo3F35pPrzF4xzRL2"
+  );
 
   console.log("NFT Minted!");
 }
